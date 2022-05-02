678ce2a8284bd5375bd75a5548316c92531fb59e
Aymen BEN MILED <benmiledaymen@hotmail.com>
2022-05-02 09:37:34 +0200

    Add Data project.

---
+++ Demo/Demo.Data/Models/Survey.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Data.Models
+{
+    public class Survey
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
