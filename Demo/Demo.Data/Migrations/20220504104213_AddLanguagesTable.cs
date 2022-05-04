3c19436aa95cc606a45364a2172be3a8906105a9
Aymen BEN MILED <benmiledaymen@hotmail.com>
2022-05-04 12:42:40 +0200

    Add language migration

---
+++ Demo/Demo.Data/Migrations/20220504104213_AddLanguagesTable.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo.Data.Migrations
+{
+    public partial class AddLanguagesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LanguagesSurvey",
+                columns: table => new
+                {
+                    LanguagesId = table.Column<int>(type: "int", nullable: false),
+                    SurveysId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LanguagesSurvey", x => new { x.LanguagesId, x.SurveysId });
+                    table.ForeignKey(
+                        name: "FK_LanguagesSurvey_Languages_LanguagesId",
+                        column: x => x.LanguagesId,
+                        principalTable: "Languages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LanguagesSurvey_Surveys_SurveysId",
+                        column: x => x.SurveysId,
+                        principalTable: "Surveys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LanguagesSurvey_SurveysId",
+                table: "LanguagesSurvey",
+                column: "SurveysId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LanguagesSurvey");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+        }
+    }
+}
